b1c3951b1a70d20bfcccc94a221a00ade49705d7
Pramod <pramod.gupta@jazeeratech.in>
2024-04-24 14:11:55 +0530

    first commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © mukesh singh All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Bluethinkinc_Stockfilter', __DIR__);
+?>
+
